84870243b7cb757baf17bb714ce54f03dd2cab45
Sylvain Colinet <scolinet@gmail.com>
2023-02-12 18:15:05 +0100

    Add copyright information on headers

--- nwasupport/generic_poll_server.c
+++ nwasupport/generic_poll_server.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2022 Sylvain "Skarsnik" Colinet.
+ *
+ * This file is orignaly part of the NWA project.
+ * (see https://github.com/usb2snes/emulator-networkaccess).
+ * Integrated in the RetroArch project
+ *
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RetroArch.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdbool.h>
 #include <stdlib.h>
